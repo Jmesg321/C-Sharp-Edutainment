803da458de90ec7d58863ec207678b36e1b309b1
Jeremiah Esguerra <81936932+Jmesg321@users.noreply.github.com>
2022-02-16 10:34:47 +0800

    Add files via upload

---
+++ Main/UIController.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+    public Image heart1, heart2, heart3;
+    public Sprite heartfull, heartEmpty, heartHalf;
+
+    public Text energyText;
+
+    public Image fadeScreen;
+    public float fadeSpeed;
+    private bool shouldFadeToBlack, shouldFadeFromBlack;
+
+    public GameObject levelCompleteText;
+
+    private void Awake()
+    {
+        instance = this;
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateEnergyCount();
+        FadeFromBlack();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldFadeToBlack)
+        {
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 1f, fadeSpeed * Time.deltaTime));
+            if (fadeScreen.color.a == 1f)
+            {
+                shouldFadeToBlack = false;
+            }
+
+        }
+        if (shouldFadeFromBlack)
+        {
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 0f, fadeSpeed * Time.deltaTime));
+            if (fadeScreen.color.a == 0f)
+            {
+                shouldFadeFromBlack = false;
+            }
+
+        }
+
+
+    }
+
+    public void UpdateHealthDisplay()
+    {
+        switch (PlayerHealthController.instance.currentHealth)
+        {
+            case 6:
+                heart1.sprite = heartfull;
+                heart2.sprite = heartfull;
+                heart3.sprite = heartfull;
+
+                break;
+            case 5:
+                heart1.sprite = heartfull;
+                heart2.sprite = heartfull;
+                heart3.sprite = heartHalf;
+
+                break;
+
+
+            case 4:
+                heart1.sprite = heartfull;
+                heart2.sprite = heartfull;
+                heart3.sprite = heartEmpty;
+
+                break;
+
+            case 3:
+                heart1.sprite = heartfull;
+                heart2.sprite = heartHalf;
+                heart3.sprite = heartEmpty;
+
+                break;
+
+            case 2:
+                heart1.sprite = heartfull;
+                heart2.sprite = heartEmpty;
+                heart3.sprite = heartEmpty;
+
+                break;
+
+            case 1:
+                heart1.sprite = heartHalf;
+                heart2.sprite = heartEmpty;
+                heart3.sprite = heartEmpty;
+
+                break;
+
+            case 0:
+                heart1.sprite = heartEmpty;
+                heart2.sprite = heartEmpty;
+                heart3.sprite = heartEmpty;
+
+                break;
+            
+            default:
+                heart1.sprite = heartEmpty;
+                heart2.sprite = heartEmpty;
+                heart3.sprite = heartEmpty;
+
+                break;
+
+        }
+    }
+
+    public void UpdateEnergyCount()
+    {
+        energyText.text = LevelManager.instance.EnergyCollected.ToString();
+    }
+
+    public void FadetoBlack()
+    {
+        shouldFadeToBlack = true;
+        shouldFadeFromBlack = false;
+    }
+    public void FadeFromBlack()
+    {
+        shouldFadeFromBlack = true;
+        shouldFadeToBlack = false;
+        
+    }
+
+}
